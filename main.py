3abe52ccde713864b627bfb714c8df670ea66ac6
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-07-19 22:00:14 +0200

    feat: demo obj

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Union, List
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import warnings
@@ -12,37 +12,71 @@ def main():
     fig = plt.figure()
     ax = fig.gca(projection='3d')
 
-    xs, ys, zs = box(0,0,0, 100)
+    # xs, ys, zs = box(0,0,0, 100)
+    obj_str = box(0,0,0, 100, 'obj')
+    with open('square_gen.obj', 'w') as f:
+        f.write(str(obj_str))
 
-    ax.plot(xs, ys, zs)
-    ax.legend()
+    # ax.plot(xs, ys, zs)
+    # ax.legend()
+    #
+    # plt.show()
 
-    plt.show()
+def box(x: int, y: int, z: int, side: int, format: str) -> Union[List[List[int]], str]:
+    if format == 'obj':
+        x1_str = f"{x:.5f}"
+        y1_str = f"{y:.5f}"
+        z1_str = f"{z:.5f}"
+        z2_str = f"{z+side:.5f}"
+        x2_str = f"{x+side:.5f}"
+        y2_str = f"{y+side:.5f}"
+        obj_str =  f'''mtllib master.mtl
+v {x1_str} {y1_str} {z1_str}
+v {x2_str} {y1_str} {z1_str}
+v {x2_str} {y2_str} {z1_str}
+v {x1_str} {y2_str} {z1_str}
 
-def box(x: int, y: int, z: int, side: int) -> List[List[int]]:
-    points =  [
-        [x,y,z],
-        [x+side,y,z],
-        [x+side,y+side,z],
-        [x,y+side,z],
-        [x,y,z],
-        [x,y,z+side],
-        [x+side,y,z+side],
-        [x+side,y,z],
-        [x+side,y,z+side],
-        [x+side,y+side,z+side],
-        [x+side,y+side,z],
-        [x+side,y+side,z+side],
-        [x,y+side,z+side],
-        [x,y+side,z],
-        [x,y+side,z+side],
-        [x,y,z+side],
-    ]
-    return [
-        [x[0] for x in points],
-        [x[1] for x in points],
-        [x[2] for x in points],
-    ]
+v {x1_str} {y1_str} {z2_str}
+v {x2_str} {y1_str} {z2_str}
+v {x2_str} {y2_str} {z2_str}
+v {x1_str} {y2_str} {z2_str}
+
+f 1 2 3 4
+f 5 6 7 8
+
+f 1 2 6 5
+f 3 4 8 7
+
+f 2 3 7 6
+f 4 1 5 8
+
+usemtl wood
+'''
+        return obj_str
+    else:
+        points =  [
+            [x,y,z],
+            [x+side,y,z],
+            [x+side,y+side,z],
+            [x,y+side,z],
+            [x,y,z],
+            [x,y,z+side],
+            [x+side,y,z+side],
+            [x+side,y,z],
+            [x+side,y,z+side],
+            [x+side,y+side,z+side],
+            [x+side,y+side,z],
+            [x+side,y+side,z+side],
+            [x,y+side,z+side],
+            [x,y+side,z],
+            [x,y+side,z+side],
+            [x,y,z+side],
+        ]
+        return [
+            [x[0] for x in points],
+            [x[1] for x in points],
+            [x[2] for x in points],
+        ]
 
 def concat_3d(head: List[List[int]], tail: List[List[int]]) -> List[List[int]]:
     return [
