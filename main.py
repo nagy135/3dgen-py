89cc260f313106d79f7c45dcd3b61fb92bbe5409
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-07-21 17:51:16 +0200

    feat: example objects

--- main.py
+++ main.py
@@ -139,17 +139,18 @@ class State:
     ) -> State:
         '''
         Creates rectangular face between given 3 corners, calculating 4th one
+        First 2 points must cross diagonally
         '''
-        mid_point = [(x + x3) / 2, (y + y3) / 2, (z + z3) / 2]
+        mid_point = [(x + x2) / 2, (y + y2) / 2, (z + z2) / 2]
         coordinates = [
             [x, y, z],
+            [
+                mid_point[0] - (x3 - mid_point[0]),
+                mid_point[1] - (y3 - mid_point[1]),
+                mid_point[2] - (z3 - mid_point[2]),
+            ],
             [x2, y2, z2],
             [x3, y3, z3],
-            [
-                mid_point[0] - (x2 - mid_point[0]),
-                mid_point[1] - (y2 - mid_point[1]),
-                mid_point[2] - (z2 - mid_point[2]),
-            ],
         ]
 
         points = [Point(x=j[0], y=j[1], z=j[2]) for j in coordinates]
@@ -200,7 +201,62 @@ def main():
 
 
 def generate_logic(state: State):
-    side = 10
+    example_predefined_points(state)
+    pass
+
+def example_predefined_points(state: State):
+    u = 28
+
+    p1 = [0,0,0]
+    p2 = [u,0,0]
+    p3 = [2*u,0,0]
+    p4 = [3*u,0,0]
+    p5 = [0,u,0]
+    p6 = [u,u,0]
+    p7 = [2*u,u,0]
+    p8 = [3*u,u,0]
+
+    p9 = [u, 0, u]
+    p10 = [2*u, 0, u]
+    p11 = [2*u, u, u]
+    p12 = [u, u, u]
+
+    p13 = [0,0,2*u]
+    p14 = [u,0,2*u]
+    p15 = [2*u,0,2*u]
+    p16 = [3*u,0,2*u]
+    p17 = [0,u,2*u]
+    p18 = [u,u,2*u]
+    p19 = [2*u,u,2*u]
+    p20 = [3*u,u,2*u]
+
+    # p21 = [u+u/3, u/3, u]
+    # p22 = [2*u-u/3, u/3, u]
+    # p23 = [2*u-u/3, 2*u/3, u]
+    # p24 = [u+u/3, 2*u/3, u]
+    #
+    # p25 = [u+u/3, u/3, 2*u]
+    # p26 = [2*u-u/3, u/3, 2*u]
+    # p27 = [2*u-u/3, 2*u/3, 2*u]
+    # p28 = [u+u/3, 2*u/3, 2*u]
+
+    state.rect(*p14, *p17, *p18)
+    state.rect(*p1, *p17, *p5)
+    state.rect(*p4, *p20, *p8)
+    state.rect(*p15, *p20, *p19)
+    state.rect(*p9, *p18, *p14)
+    state.rect(*p10, *p19, *p11)
+    state.rect(*p9, *p11, *p12)
+    state.rect(*p1, *p14, *p13)
+    state.rect(*p3, *p16, *p4)
+    state.rect(*p2, *p10, *p9)
+    state.rect(*p5, *p18, *p6)
+    state.rect(*p7, *p20, *p19)
+    state.rect(*p6, *p11, *p7)
+    state.rect(*p1, *p8, *p4)
+
+def example_boxy_s(state: State):
+    side = 2
     state.box(0, 0, 0, side)
     state.box(0, 0, side, side)
     state.box(side, 0, 0, side)
